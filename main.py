8a66190cdc775b63ecaedc68147fe933c223e6ee
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-29 23:49:26 -0500

    creatinghtml templates

--- main.py
+++ main.py
@@ -2,13 +2,13 @@
 #to install a package, we'll use the command 'pip3 install ${packageName}'
 #we installed both flask-sqlalchemy and flask as packages here
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, world!"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
