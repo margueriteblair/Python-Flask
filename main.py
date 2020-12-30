e66ea84566f432ce300e25f612a00e8b8b95c045
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-29 23:45:04 -0500

    fleshing out main file to use flask

--- main.py
+++ main.py
@@ -2,3 +2,6 @@
 #to install a package, we'll use the command 'pip3 install ${packageName}'
 #we installed both flask-sqlalchemy and flask as packages here
 
+from flask import Flask
+
+app = Flask(__name__)
