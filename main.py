3b3ec5a93d9fb9864687e45d4af2530ef67c555f
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-31 13:45:32 -0500

    troubleshoting

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def index():
 
     else:
         tasks = Todo.query.order_by(Todo.date_created).all()
-        return render_template('index.html')
+        return render_template('index.html', tasks=tasks)
 
 
 if __name__ == "__main__":
